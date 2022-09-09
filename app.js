b6bdb5489207726f83fbfbc7d6108bf9c9e98adc
belgonzalez21 <nsbelg@gmail.com>
2022-09-09 11:38:12 -0300

    mvc terminado

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-const express =require('express');
+const express = require('express');
 const app = express();
+const path = require('path');
+const PORT = 3030;
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-    });
+/*IMPORTACIÓN DE ROUTE*/
+const mainRouter = require("./routes/main");
 
-    app.set('view engine', 'ejs');
-    app.use('/', indexRouter);
+/*ENGINE*/
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+/*DIRECCIÓN DE ROUTES*/
+app.use("/", mainRouter);
+
+/*PORT*/
+app.listen(3030, () => {
+  console.log(`Servidor funcionando en http://localhost:${PORT}`);
+});
+
+
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,3 +1,8 @@
-module.exports={
-    main : (req, res) => res.render('home')
+module.exports = {
+    home: (req, res) => {
+        return res.render('home')
+    },
+    about: (req, res) => {
+        return res.render('about')
+    }
 }
